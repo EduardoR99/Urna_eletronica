96730bbca10616ae52188d3ee3cea685f52d2c27
EduardoR99 <102009964+EduardoR99@users.noreply.github.com>
2022-06-01 02:50:07 -0300

    first deploy

---
+++ README.md
@@ -0,0 +1,8 @@
+# Urna Eletrônica
+
+Projeto de front-end para reproduzir uma urna eletrônica, feito inteiramente em HTML, Javascript e CSS. Projeto desenvolvido durante
+o curso de Javascript [b7web](https://b7web.com.br)
+
+[Clique aqui](https://frontend-pizza-site.netlify.app) para ir
+
+![App Images](/assets/images/1.png) 
---
+++ assets/js/etapas.js
@@ -0,0 +1,49 @@
+let etapas = [{
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+        {
+            numero: '38111',
+            name: 'Fulano de Tal',
+            partido: 'ABC',
+            fotos: [
+                { url: './assets/images/38111.jpg', legenda: 'Vereador', small: false }
+            ]
+        },
+        {
+            numero: '77222',
+            name: 'Beltrano da Silva',
+            partido: 'DEFG',
+            fotos: [
+                {url: './assets/images/77222.jpg', legenda: 'Vereador'}
+            ]
+        }
+    ]
+},
+{
+    titulo: 'Prefeito',
+    numeros: 2,
+    candidatos: [
+        {
+            numero: '99',
+            name: 'Ciclano',
+            partido: 'ABC',
+            vice: 'Cic',
+            fotos: [
+                { url: './assets/images/99.jpg', legenda: 'Prefeito'},
+                { url: './assets/images/99_2.jpg', legenda: ' Vice Prefeito', small: true}
+            ]
+        },
+        {
+            numero: '84',
+            name: 'Zulano',
+            partido: 'QWERTY',
+            vice: 'Zul',
+            fotos: [
+                {url: './assets/images/84.jpg', legenda: 'Prefeito'},
+                {url: './assets/images/84_2.jpg', legenda: 'Vice Prefeito', small: true}
+            ]
+        }
+    ]
+}
+]
